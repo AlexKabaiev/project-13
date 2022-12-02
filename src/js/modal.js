ee41616f87ad1db612c7a48f9871a7c0ec6e4fbd
Andrii-Verhun <113340615+Andrii-Verhun@users.noreply.github.com>
2022-12-02 15:09:54 +0200

    Финиш все модалки

--- src/js/modal.js
+++ src/js/modal.js
@@ -27,3 +27,18 @@
     refs.modal.classList.toggle("is-hidden");
   }
 })();
+
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[data-modal-price-open]"),
+    closeModalBtn: document.querySelector("[data-modal-price-close]"),
+    modal: document.querySelector("[data-modal-price]"),
+  };
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle("is-hidden");
+  }
+})();
---
+++ src/js/select-price.js
@@ -0,0 +1,8 @@
+const defaultSelectprice = () => {
+    const element = document.querySelector('.select-amount-night');
+    const choices = new Choices(element, {
+        searchEnabled: false,
+        itemSelectText: '',
+    });
+};
+defaultSelectprice();
