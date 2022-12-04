62a5fc51597009700d798277b2d61d16187f1d69
Andrii-Verhun <113340615+Andrii-Verhun@users.noreply.github.com>
2022-12-04 12:06:19 +0200

    Скрипт

--- src/index.js
+++ src/index.js
@@ -1,51 +1,3 @@
-//------------------modal---------------------
-
-
-(() => {
-  const refs = {
-    openModalBtn: document.querySelector("[data-modal-table-open]"),
-    closeModalBtn: document.querySelector("[data-modal-table-close]"),
-    modal: document.querySelector("[data-modal-table]"),
-  };
-
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
-
-  function toggleModal() {
-    refs.modal.classList.toggle("is-hidden");
-  }
-})();
-
-(() => {
-  const refs = {
-    openModalBtn: document.querySelector("[data-modal-room-open]"),
-    closeModalBtn: document.querySelector("[data-modal-room-close]"),
-    modal: document.querySelector("[data-modal-room]"),
-  };
-
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
-
-  function toggleModal() {
-    refs.modal.classList.toggle("is-hidden");
-  }
-})();
-
-(() => {
-  const refs = {
-    openModalBtn: document.querySelector("[data-modal-price-open]"),
-    closeModalBtn: document.querySelector("[data-modal-price-close]"),
-    modal: document.querySelector("[data-modal-price]"),
-  };
-
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
-
-  function toggleModal() {
-    refs.modal.classList.toggle("is-hidden");
-  }
-})();
-
 //--------------------imask-------------------------------
 
 
@@ -4688,3 +4640,52 @@ const defaultSelectprice = () => {
     });
 };
 defaultSelectprice();
+
+
+//------------------modal---------------------
+
+
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[data-modal-table-open]"),
+    closeModalBtn: document.querySelector("[data-modal-table-close]"),
+    modal: document.querySelector("[data-modal-table]"),
+  };
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle("is-hidden");
+  }
+})();
+
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[data-modal-room-open]"),
+    closeModalBtn: document.querySelector("[data-modal-room-close]"),
+    modal: document.querySelector("[data-modal-room]"),
+  };
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle("is-hidden");
+  }
+})();
+
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[data-modal-price-open]"),
+    closeModalBtn: document.querySelector("[data-modal-price-close]"),
+    modal: document.querySelector("[data-modal-price]"),
+  };
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle("is-hidden");
+  }
+})();
