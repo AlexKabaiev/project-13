907bacb6e4c3d046444d7b5038a63753da06ff64
AlexKabaiev <113040507+AlexKabaiev@users.noreply.github.com>
2022-12-06 20:45:24 +0000

    Deploying to gh-pages from @ AlexKabaiev/project-13@64f20943e8a912dbbce49c894d66119aebfc55f9 🚀

---
+++ index.af20264e.js
@@ -0,0 +1,2 @@
+new Swiper(".swiper-details-hotel",{direction:"horizontal",loop:!0,centeredSlides:!0,breakpoints:{414:{slidesPerView:1.29,spaceBetween:20},1024:{slidesPerView:"auto",spaceBetween:34},1280:{slidesPerView:"auto",spaceBetween:86}},pagination:{el:".swiper-pagination"},navigation:{nextEl:".swiper-button-next1",prevEl:".swiper-button-prev1"},scrollbar:{el:".swiper-scrollbar"}});
+//# sourceMappingURL=index.af20264e.js.map
