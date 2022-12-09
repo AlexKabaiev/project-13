8ad91e3821bf2bd28c8855e1b6359579495a80bf
AlexKabaiev <113040507+AlexKabaiev@users.noreply.github.com>
2022-12-09 12:04:56 +0000

    Deploying to gh-pages from @ AlexKabaiev/project-13@7cad1d11f38916c530c84ca21a332589617f9145 🚀

---
+++ index.f354b4f8.js
@@ -0,0 +1,2 @@
+new Swiper(".about-restaurant__slaider",{direction:"horizontal",loop:!0,breakpoints:{414:{slidesPerView:"auto",spaceBetween:20},1024:{slidesPerView:"auto",spaceBetween:40},1280:{slidesPerView:"auto",spaceBetween:40}},pagination:{el:".swiper-pagination"},navigation:{nextEl:".swiper-button-start",prevEl:".swiper-button-finish"},scrollbar:{el:".swiper-scrollbar"}});
+//# sourceMappingURL=index.f354b4f8.js.map
