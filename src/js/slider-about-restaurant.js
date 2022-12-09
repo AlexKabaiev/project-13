03748e7bb90f3b180853097587613545c4c7ba78
Alexey Kabaiev <kabaievalex@gmail.com>
2022-12-09 12:00:23 +0200

    temp_comit

--- src/js/slider-about-restaurant.js
+++ src/js/slider-about-restaurant.js
@@ -9,33 +9,33 @@ const swiper = new Swiper('.swiper-about-restaurant', {
     // адаптив для разрешения экрана
     414: {
       slidesPerView: 'auto',
-          spaceBetween: 20
+          spaceBetween: 20,
     },
     // when window width is >= 
     1024: {
       slidesPerView: 'auto',
-      spaceBetween: 40
+      spaceBetween: 40,
     },
     // when window width is >=
     1280: {
       slidesPerView: 'auto',
-      spaceBetween: 40
+      spaceBetween: 40,
     }
   },
 
   // If we need pagination
   pagination: {
-    el: '.swiper-pagination'
+    el: '.swiper-pagination',
   },
 
   // Navigation arrows
   navigation: {
     nextEl: '.swiper-button-start',
-    prevEl: '.swiper-button-finish'
+    prevEl: '.swiper-button-finish',
   },
 
   // And if we need scrollbar
   scrollbar: {
-    el: '.swiper-scrollbar'
+    el: '.swiper-scrollbar',
   },
 });
